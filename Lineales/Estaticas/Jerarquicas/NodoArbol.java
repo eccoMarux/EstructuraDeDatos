87c9c63e3661de578626258f3b9b4c249420ebef
eccoMarux <maria.monserrat@est.fi.uncoma.edu.ar>
2024-04-18 11:37:03 -0300

    Actualizacion ArbolBin

---
+++ Lineales/Estaticas/Jerarquicas/NodoArbol.java
@@ -0,0 +1,40 @@
+package Lineales.Estaticas.Jerarquicas;
+
+public class NodoArbol {
+
+    private Object elem;
+    private NodoArbol izquierdo;
+    private NodoArbol derecho;
+
+    public NodoArbol(Object elem, NodoArbol izquierdo, NodoArbol derecho) {
+        this.elem = elem;
+        this.izquierdo = izquierdo;
+        this.derecho = derecho;
+    }
+
+    //modificadoras
+    public void setElem(Object elem){
+        this.elem = elem;
+    }
+
+    public void setIzquiedo(NodoArbol izquierdo){
+        this.izquierdo = izquierdo;
+    }
+
+    public void setDerecho(NodoArbol derecho){
+        this.derecho = derecho;
+    }
+
+    //Observadoras
+    public Object getElem(){
+        return elem;
+    }
+
+    public NodoArbol getIzquierdo(){
+        return izquierdo;
+    }
+
+    public NodoArbol getDerecho(){
+        return derecho;
+    }
+}
